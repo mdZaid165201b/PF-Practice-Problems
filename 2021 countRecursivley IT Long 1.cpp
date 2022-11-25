1e25001eac8da616e0cae54945f7170d374a87b2
Muhammad Zaid <107930247+mdZaid165201b@users.noreply.github.com>
2022-11-25 14:00:16 +0500

    Update 2021 countRecursivley IT Long 1.cpp

--- 2021 countRecursivley IT Long 1.cpp
+++ 2021 countRecursivley IT Long 1.cpp
@@ -18,6 +18,9 @@ int countNumber(int arr[], int num, int len, int count = 0){
 
 
 int main(){
+  int arr[6] = {2,4,2,4,5,2};
+  int len = sizeof(arr) / sizeof(arr[0]);
+  console.log(arr, 2, len);
   return 0;
 }
 
